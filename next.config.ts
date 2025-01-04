0f4e1da4783bfaea7a60dbe1faa7187ed790d308
sohambasu963 <sohambasu963@gmail.com>
2025-01-03 19:02:11 -0500

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: "all",
+      maxSize: 200000,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
